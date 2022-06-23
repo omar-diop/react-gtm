53f064cf3ef976bdcae275a3d2d2f85463fff9c8
Omar Diop <accounts@omardiop.com>
2022-06-23 12:31:29 +0200

    Added consent mode settings to Tag Manager initialization

--- src/TagManager.js
+++ src/TagManager.js
@@ -1,53 +1,61 @@
-import Snippets from './Snippets'
+import Snippets from "./Snippets";
 
 const TagManager = {
   dataScript: function (dataLayer) {
-    const script = document.createElement('script')
-    script.innerHTML = dataLayer
-    return script
+    const script = document.createElement("script");
+    script.innerHTML = dataLayer;
+    return script;
   },
   gtm: function (args) {
-    const snippets = Snippets.tags(args)
+    const snippets = Snippets.tags(args);
 
     const noScript = () => {
-      const noscript = document.createElement('noscript')
-      noscript.innerHTML = snippets.iframe
-      return noscript
-    }
+      const noscript = document.createElement("noscript");
+      noscript.innerHTML = snippets.iframe;
+      return noscript;
+    };
 
     const script = () => {
-      const script = document.createElement('script')
-      script.innerHTML = snippets.script
-      return script
-    }
+      const script = document.createElement("script");
+      script.innerHTML = snippets.script;
+      return script;
+    };
+    const consentScript = () => {
+      const script = document.createElement("script");
+      script.innerHTML = snippets.consentScript;
+      return script;
+    };
 
-    const dataScript = this.dataScript(snippets.dataLayerVar)
+    const dataScript = this.dataScript(snippets.dataLayerVar);
 
     return {
       noScript,
       script,
-      dataScript
-    }
+      consentScript,
+      dataScript,
+    };
   },
-  initialize: function ({ gtmId, events = {}, dataLayer, dataLayerName = 'dataLayer', auth = '', preview = '' }) {
+  initialize: function ({ gtmId, events = {}, dataLayer, dataLayerName = "dataLayer", auth = "", preview = "", consentSettings }) {
     const gtm = this.gtm({
       id: gtmId,
       events: events,
       dataLayer: dataLayer || undefined,
+      consentSettings: consentSettings,
       dataLayerName: dataLayerName,
       auth,
-      preview
-    })
-    if (dataLayer) document.head.appendChild(gtm.dataScript)
-    document.head.insertBefore(gtm.script(), document.head.childNodes[0])
-    document.body.insertBefore(gtm.noScript(), document.body.childNodes[0])
+      preview,
+    });
+    if (dataLayer) document.head.appendChild(gtm.dataScript);
+    document.head.insertBefore(gtm.script(), document.head.childNodes[0]);
+    document.head.insertBefore(gtm.consentScript(), document.head.childNodes[0]);
+    document.body.insertBefore(gtm.noScript(), document.body.childNodes[0]);
   },
-  dataLayer: function ({dataLayer, dataLayerName = 'dataLayer'}) {
-    if (window[dataLayerName]) return window[dataLayerName].push(dataLayer)
-    const snippets = Snippets.dataLayer(dataLayer, dataLayerName)
-    const dataScript = this.dataScript(snippets)
-    document.head.insertBefore(dataScript, document.head.childNodes[0])
-  }
-}
+  dataLayer: function ({ dataLayer, dataLayerName = "dataLayer" }) {
+    if (window[dataLayerName]) return window[dataLayerName].push(dataLayer);
+    const snippets = Snippets.dataLayer(dataLayer, dataLayerName);
+    const dataScript = this.dataScript(snippets);
+    document.head.insertBefore(dataScript, document.head.childNodes[0]);
+  },
+};
 
-module.exports = TagManager
+module.exports = TagManager;
